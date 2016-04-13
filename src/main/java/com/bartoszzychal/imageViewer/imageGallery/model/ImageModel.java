f34ca4487a81c5c3a119bf8aa5acd12a68d7b97b
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-04-13 15:56:19 +0200

    added check if file exists and closed inputstream

--- src/main/java/com/bartoszzychal/imageViewer/imageGallery/model/ImageModel.java
+++ src/main/java/com/bartoszzychal/imageViewer/imageGallery/model/ImageModel.java
@@ -2,7 +2,7 @@ package com.bartoszzychal.imageViewer.imageGallery.model;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -11,38 +11,40 @@ public class ImageModel {
 	private Image smallImage;
 	private Image normalImage;
 	private File file;
-	
+
 	public ImageModel(File file) {
 		this.file = file;
 	}
 
-	public ImageView getSmallImageView(){
+	public ImageView getSmallImageView() {
 		ImageView imageView = null;
-		if(smallImage == null){
-			try {
-				smallImage = new Image(new FileInputStream(file), 150, 0, true, true);
+		if (smallImage == null && file.exists()) {
+
+			try (FileInputStream fileInputStream = new FileInputStream(file)) {
+				smallImage = new Image(fileInputStream, 150, 0, true, true);
 				imageView = new ImageView(smallImage);
 				imageView.setFitWidth(150);
-			} catch (FileNotFoundException e) {
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
+
 		}
 		return imageView;
 	}
-	
-	public Image getRealSizeImage(){
-		if(normalImage == null){
-			try {
-				normalImage = new Image(new FileInputStream(file));
-			} catch (FileNotFoundException e) {
+
+	public Image getRealSizeImage() {
+		if (normalImage == null && file.exists()) {
+			try(FileInputStream fileInputStream = new FileInputStream(file)) {
+				normalImage = new Image(fileInputStream);
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 		return normalImage;
 	}
-	
-	public String getName(){
+
+	public String getName() {
 		return file.getName();
 	}
-	
+
 }
